55ad485f947d945b054e2d314095f461712341a0
Sam Roeca <samuel.roeca@gmail.com>
2019-02-10 18:28:44 -0500

    Update hooks with more helpful message

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo "1. Create & activate a Python virtual environment"
-echo "2. Run 'make setup'"
-echo "3. [Optional] git init"
+echo "***************************************************"
+echo "* Success! Some next steps:"
+echo "***************************************************"
+echo "* 1. Create & activate a Python virtual environment"
+echo "* 2. Run 'make setup'"
+echo "***************************************************"
